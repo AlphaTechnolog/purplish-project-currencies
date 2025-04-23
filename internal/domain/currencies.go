146f25e53c55b3a3b885eb85d7b9bb5a99cfb841
Gabriel Guerra <gfranklings@gmail.com>
2025-04-23 16:31:00 -0400

    chore: fmt

--- internal/domain/currencies.go
+++ internal/domain/currencies.go
@@ -3,16 +3,16 @@ package domain
 import "github.com/google/uuid"
 
 type Currency struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
 	Description *string `json:"description"`
 }
 
 type CurrencyCompany struct {
-	CompanyID string `json:"company_id"`
-	CurrencyID string `json:"currency_id"`
+	CompanyID    string `json:"company_id"`
+	CurrencyID   string `json:"currency_id"`
 	CurrencyName string `json:"currency_name"`
-	ExchangeRate int `json:"exchange_rate"`
+	ExchangeRate int    `json:"exchange_rate"`
 }
 
 func (cc *CurrencyCompany) ValidateUUIDs() error {
